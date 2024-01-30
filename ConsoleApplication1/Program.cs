4e99251bcd571aaa90a6e298f5253430773ecd34
Daniel Ataiyero <danielataiyero@Daniels-MacBook-Pro.local>
2024-01-30 17:21:08 +0000

    added rectangle to customer selection

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -7,58 +7,68 @@ namespace ConsoleApplication1 // Note: actual namespace depends on the project n
     {
         public static void Main(string[] args)
         {
+            
+            const double HOUR_RATE = 86;
+            const double SQUARE_FEET = 20;
+           
+            const char SHAPE_CIRCLE = 'c';
+            const char SHAPE_TRIANGLE = 't';
+            const char SHAPE_RECTANGLE = 'r';
+            
             Console.WriteLine("TileCost Calculator"); 
             
-            Console.WriteLine("Please enter width that is needed?");
-            string requestedWidth = Console.ReadLine();
-            
-            Console.WriteLine("Please enter length that is needed?");
-            string requestedLength = Console.ReadLine();
-            
-            Console.WriteLine("Please enter the cost per flooring that is needed?");
-            string costPerUnit = Console.ReadLine();
-            
-            Int32.TryParse(requestedWidth, out int width);
-            Int32.TryParse(requestedLength, out int length);
-            Int32.TryParse(costPerUnit, out int cost);
-            
-            int total = width * length * cost;
-            
-            Console.WriteLine("Total cost is £" + total);
-            
-            // Additional difficulty
-            const double HOUR_RATE = 86;
-            const double SQAURE_FEET = 20;
-            const string TRIANGLE = "Triangle";
-            const string CIRCLE = "Circle";
-            
-            double calculateShape = 0;
-            
-            
-            Console.WriteLine("Please select what shape you would like to calculate: (T) for Triangle or (C) for Circle: ");
+            Console.WriteLine("Please select what shape you would like to calculate: (T) for Triangle, (R) for Rectangle or (C) for Circle: ");
             string selectedShape = Console.ReadLine().ToLower();
-            
-            string shape = selectedShape == "c" ? CIRCLE : TRIANGLE;
-             
-            Console.Write("Please enter " + shape +" width: ");
-            double shapeWidth = Convert.ToDouble(Console.ReadLine());
-            
-            Console.Write("Please enter " + shape + " length: ");
-            double shapeLength = Convert.ToDouble(Console.ReadLine());
+            char shapeCharacter = selectedShape.ToCharArray()[0];
+            double calculateShape = 0;
 
-            if (shape == CIRCLE)
+            
+            if (SHAPE_CIRCLE == shapeCharacter)
             {
-                calculateShape = shapeWidth * shapeLength;
+                Console.Write("Please enter Circle radius: ");
+                string requestedRadius = Console.ReadLine();
+                
+                Int32.TryParse(requestedRadius, out int radius);
+                
+                calculateShape = Math.PI * radius * radius;
             }
-            if(shape == TRIANGLE)
+            if(SHAPE_TRIANGLE == shapeCharacter)
             {
-                calculateShape = 0.5 * shapeWidth * shapeLength;
+                Console.Write("Please enter Triangle base: ");
+                string requestedBase = Console.ReadLine();
+                Console.Write("Please enter Triangle height: ");
+                string requestedHeight = Console.ReadLine();
+                
+                Int32.TryParse(requestedBase, out int baseNeeded);
+                Int32.TryParse(requestedHeight, out int height);
+                
+                calculateShape = 0.5 * baseNeeded * height;
+            }
+            if(SHAPE_RECTANGLE == shapeCharacter)
+            {
+                Console.Write("Please enter Rectangle width: ");
+                string requestedWidth = Console.ReadLine();
+                Console.Write("Please enter Rectangle length: ");
+                string requestedLength = Console.ReadLine();
+                
+                Int32.TryParse(requestedWidth, out int width);
+                Int32.TryParse(requestedLength, out int length);
+                
+                calculateShape = width * length;
             }
             
-            Console.WriteLine("The " + shape +" size is " + calculateShape);
+            Console.WriteLine("The selected shape size is " + Math.Round(calculateShape,2) + ".");
 
-            double jobCosts = calculateShape / SQAURE_FEET * HOUR_RATE;
-            Console.WriteLine("Total cost of labor for installing " + shape +" is £" + jobCosts + ".");
+            Console.WriteLine("Please enter the cost per unit of flooring: ");
+            string costPerUnit = Console.ReadLine();
+            Int32.TryParse(costPerUnit, out int cost);
+
+            double totalCost = calculateShape / SQUARE_FEET * cost;
+            Console.WriteLine("Total cost for the flooring is £" + Math.Round(totalCost,2));
+
+            double jobCosts = calculateShape / SQUARE_FEET * HOUR_RATE;
+            Console.WriteLine("Total cost of labor for installing the flooring is £" + Math.Round(jobCosts,2));
+
         }
     }
 }
